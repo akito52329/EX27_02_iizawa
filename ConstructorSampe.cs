91206b56147810381ef3bfa9229275d71ec9c433
sakuseimoto@gmail.com <sakuseimoto@gmail.com>
2021-11-25 13:32:36 +0900

    プロジェクト ファイルを追加します。

---
+++ ConstructorSampe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ConsoleUtility;
+
+namespace ConstructorSampe
+{
+    class ConstructorSampeMain
+    {
+        static void Main(string[] args)
+        {
+            Triangle triangle = new Triangle(
+                InteractiveInput.InputFloat("三角形の時計回りにある1つ目の辺の長さ："),
+                InteractiveInput.InputFloat("三角形の時計回りにある2つ目の辺の長さ："),
+                InteractiveInput.InputFloat("三角形の時計回りにある3つ目の辺の長さ："));
+
+            Console.WriteLine($"面積は{triangle.GetSurface()}");
+        }
+    }
+}
---
+++ Triangle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConstructorSampe
+{
+    class Triangle
+    {
+        private float a;
+        private float b;
+        private float c;
+        private float s;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        public Triangle(float a, float b, float c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+
+        /// <summary>
+        /// 三角形の面積
+        /// </summary>
+        /// <returns>計算</returns>
+        public double GetSurface()
+        {
+            s = (a + b + c) / 2;
+            return Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+        }
+    }
+}
